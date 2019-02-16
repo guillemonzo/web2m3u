26833490abef86934c61745fbd6427923de83770
Guille <guillemonzo@gmail.com>
2019-02-16 21:34:03 +0100

    First commit.

---
+++ src/test/java/com/guimonel/web2m3u/Web2m3uApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guimonel.web2m3u;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Web2m3uApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
